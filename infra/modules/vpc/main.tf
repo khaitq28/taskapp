ac4a5f939b8c58aa309241c35fbca78026123407
TRAN Quang Khai <canhcung88@gmail.com>
2025-10-05 22:05:27 +0200

    init terraform

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,89 @@
+variable "env" {
+  type = string
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.env == "prod" ? "10.0.0.0/16" : var.env == "uat" ? "10.1.0.0/16" : "10.2.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc-${var.env}-taskapp"
+    Environment = var.env
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone = element(["eu-west-3a", "eu-west-3b"], count.index)
+  tags = {
+    Name = "public-subnet-${var.env}-${count.index}"
+    Environment = var.env
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
+  availability_zone = element(["eu-west-3a", "eu-west-3b"], count.index)
+  tags = {
+    Name = "private-subnet-${var.env}-${count.index}"
+    Environment = var.env
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "igw-${var.env}-taskapp"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "rt-public-${var.env}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "nat-${var.env}-taskapp"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "rt-private-${var.env}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
